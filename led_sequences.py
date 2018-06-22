67b1fd1a2cca8403c767cefcea575a4458ba2feb
electromantis79 <36968910+electromantis79@users.noreply.github.com>
2018-06-22 09:51:05 -0400

    update led sequence times

--- led_sequences.py
+++ led_sequences.py
@@ -13,31 +13,31 @@ class LedSequences(object):
 		"""ONE SHOT - Self Stop"""
 		if enable:
 			read = self.timer.read_ms()
-			if 0 <= read < 250:
+			if 0 <= read < 200:
 				print('[Clock Running] = ON', read, 'ms')
-			elif 250 <= read < 300:
+			elif 200 <= read < 400:
 				print('[Signal Strength] = ON', read, 'ms')
-			elif 300 <= read < 500:
+			elif 400 <= read < 600:
 				print('[Clock Running] = OFF', read, 'ms')
-			elif 500 <= read < 750:
+			elif 600 <= read < 800:
 				print('[Battery Strength] = ON', read, 'ms')
-			elif 750 <= read < 1000:
+			elif 800 <= read < 1000:
 				print('[Strength Bar 1] = ON', read, 'ms')
-			elif 1000 <= read < 1250:
+			elif 1000 <= read < 1200:
 				print('[Strength Bar 2] = ON', read, 'ms')
-			elif 1250 <= read < 1500:
+			elif 1200 <= read < 1400:
 				print('[Strength Bar 3] = ON', read, 'ms')
-			elif 1500 <= read < 1750:
+			elif 1400 <= read < 2000:
 				print('[Strength Bar 4] = ON', read, 'ms')
-			elif 1750 <= read < 2000:
+			elif 2000 <= read < 2200:
 				print('[Signal Strength] = OFF', read, 'ms')
-			elif 2000 <= read < 2250:
+			elif 2200 <= read < 2400:
 				print('[Battery Strength] = OFF', read, 'ms')
-			elif 2250 <= read < 2500:
+			elif 2400 <= read < 2600:
 				print('[Strength Bar 1] = OFF', read, 'ms')
-			elif 2500 <= read < 2750:
+			elif 2600 <= read < 2800:
 				print('[Strength Bar 2] = OFF', read, 'ms')
-			elif 2750 <= read < 3000:
+			elif 2800 <= read < 3000:
 				print('[Strength Bar 3] = OFF', read, 'ms')
 			elif 3000 <= read:
 				print('[Strength Bar 4] = OFF', read, 'ms')
@@ -51,22 +51,22 @@ class LedSequences(object):
 		"""ONE SHOT - Self Stop"""
 		if enable:
 			read = self.timer.read_ms()
-			if 0 <= read < 250:
+			if 0 <= read < 400:
 				print('[Clock Running] = [Signal Strength] = [Battery Strength] = OFF', read, 'ms')
 				print('[Strength Bar 1] = [Strength Bar 2] = [Strength Bar 3] = [Strength Bar 4] = ON')
-			elif 250 <= read < 500:
+			elif 400 <= read < 800:
 				print('[Signal Strength] = ON', read, 'ms')
-			elif 500 <= read < 750:
+			elif 800 <= read < 1000:
 				print('[Battery Strength] = ON', read, 'ms')
-			elif 750 <= read < 1000:
+			elif 1000 <= read < 1200:
 				print('[Signal Strength] = OFF', read, 'ms')
-			elif 1000 <= read < 1250:
+			elif 1200 <= read < 1400:
 				print('[Battery Strength] = OFF', read, 'ms')
-			elif 1250 <= read < 1500:
+			elif 1400 <= read < 1600:
 				print('[Strength Bar 4] = OFF', read, 'ms')
-			elif 1500 <= read < 1750:
+			elif 1600 <= read < 1800:
 				print('[Strength Bar 3] = OFF', read, 'ms')
-			elif 1750 <= read < 2000:
+			elif 1800 <= read < 2000:
 				print('[Strength Bar 2] = OFF', read, 'ms')
 			elif 2000 <= read:
 				print('[Strength Bar 1] = OFF', read, 'ms')
@@ -80,23 +80,23 @@ class LedSequences(object):
 		"""Repeat this method until it's time to go to sleep or until a Receiver Device is discovered."""
 		if enable:
 			read = self.timer.read_ms()
-			if 0 <= read < 250:
+			if 0 <= read < 200:
 				print('All LEDs = OFF', read, 'ms')
-			elif 250 <= read < 750:
+			elif 200 <= read < 700:
 				print('[Signal Strength] = ON', read, 'ms')
-			elif 750 <= read < 1000:
+			elif 700 <= read < 1000:
 				print('[Strength Bar 1] = ON', read, 'ms')
-			elif 1000 <= read < 1250:
+			elif 1000 <= read < 1200:
 				print('[Signal Strength] = OFF', read, 'ms')
-			elif 1250 <= read < 1500:
+			elif 1200 <= read < 1500:
 				print('[Strength Bar 2] = ON', read, 'ms')
-			elif 1500 <= read < 1750:
+			elif 1500 <= read < 1700:
 				print('[Strength Bar 1] = OFF', read, 'ms')
-			elif 1750 <= read < 2000:
+			elif 1700 <= read < 2000:
 				print('[Strength Bar 3] = ON', read, 'ms')
-			elif 2000 <= read < 2250:
+			elif 2000 <= read < 2200:
 				print('[Strength Bar 2] = OFF', read, 'ms')
-			elif 2250 <= read < 2500:
+			elif 2200 <= read < 2500:
 				print('[Strength Bar 4] = ON', read, 'ms')
 			elif 2500 <= read < 3000:
 				print('[Strength Bar 3] = OFF', read, 'ms')
@@ -111,18 +111,18 @@ class LedSequences(object):
 		"""ONE SHOT - Self Stop"""
 		if enable:
 			read = self.timer.read_ms()
-			if 0 <= read < 250:
+			if 0 <= read < 300:
 				print('[Clock Running] = [Battery Strength] = OFF', read, 'ms')
 				print('[Signal Strength] = [Bar 1] = [Bar 2] = [Bar 3] = [Bar 4] = ON')
-			elif 250 <= read < 500:
+			elif 300 <= read < 500:
 				print('[Signal Strength] = [Bar 1] = [Bar 2] = [Bar 3] = [Bar 4] = OFF', read, 'ms')
-			elif 500 <= read < 750:
+			elif 500 <= read < 800:
 				print('[Signal Strength] = [Bar 1] = [Bar 2] = [Bar 3] = [Bar 4] = ON', read, 'ms')
-			elif 750 <= read < 1000:
+			elif 800 <= read < 1000:
 				print('[Signal Strength] = [Bar 1] = [Bar 2] = [Bar 3] = [Bar 4] = OFF', read, 'ms')
-			elif 1000 <= read < 1250:
+			elif 1000 <= read < 1300:
 				print('[Signal Strength] = [Bar 1] = [Bar 2] = [Bar 3] = [Bar 4] = ON', read, 'ms')
-			elif 1250 <= read < 1500:
+			elif 1300 <= read < 1500:
 				print('[Signal Strength] = [Bar 1] = [Bar 2] = [Bar 3] = [Bar 4] = OFF', read, 'ms')
 			elif 1500 <= read:
 				print('Normal Signal Strength Mode', read, 'ms')
@@ -159,15 +159,15 @@ class LedSequences(object):
 		"""Repeat toggle the [Signal Strength] and [Bar 4] LEDs for one cycle or until transfer is complete"""
 		if enable:
 			read = self.timer.read_ms()
-			if 0 <= read < 250:
+			if 0 <= read < 300:
 				print('[Battery Strength] = [Clock Running] = OFF', read, 'ms')
 				print('[Bar 1] = [Bar 2] = [Bar 3] = [Bar 4] = OFF')
 				print('[Signal Strength] = ON')
-			elif 250 <= read < 500:
+			elif 300 <= read < 500:
 				print('[Bar 4] = ON', read, 'ms')
-			elif 500 <= read < 750:
+			elif 500 <= read < 800:
 				print('[Signal Strength] = OFF', read, 'ms')
-			elif 750 <= read < 1000:
+			elif 800 <= read < 1000:
 				print('[Bar 4] = OFF', read, 'ms')
 			elif 1000 <= read:
 				self.timer.reset()
@@ -183,13 +183,13 @@ class LedSequences(object):
 			read = self.timer.read_ms()
 			if 0 <= read < 1000:
 				print('All LEDs = OFF', read, 'ms')
-			elif 1000 <= read < 1750:
+			elif 1000 <= read < 1800:
 				print('[Clock Running] = ON', read, 'ms')
-			elif 1750 <= read < 3000:
+			elif 1800 <= read < 3000:
 				print('[Clock Running] = OFF', read, 'ms')
-			elif 3000 <= read < 3750:
+			elif 3000 <= read < 3800:
 				print('[Clock Running] = ON', read, 'ms')
-			elif 3750 <= read:
+			elif 3800 <= read:
 				print('[Clock Running] = OFF', read, 'ms')
 				self.timer.reset()
 				print('time_of_day_sequence END')
